83eaa08ccaa59198d8c4049a2aa9956dd3a55e17
SergiooCoorne <Sergio.Cornejo@alu.uclm.es>
2023-12-19 12:32:31 +0100

    Clases asociaadas al segundo problema

---
+++ Testing_P2/src/main/java/EcuacionSegundoGrado.java
@@ -0,0 +1,15 @@
+public class EcuacionSegundoGrado {
+	public double[] calcularRaices(double a, double b, double c) throws Exception {
+		double discriminante = b * b - 4 * a * c;
+		if (discriminante > 0) {
+			double x1 = (-b + Math.sqrt(discriminante)) / (2 * a);
+			double x2 = (-b - Math.sqrt(discriminante)) / (2 * a);
+			return new double[] { x1, x2 };
+		} else if (discriminante == 0) {
+			double x = -b / (2 * a);
+			return new double[] { x };
+		} else {
+			throw new Exception("La ecuacion no tiene raices reales.");
+		}
+	}
+}
---
+++ Testing_P2/src/main/java/InterfazUsuario.java
@@ -0,0 +1,38 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class InterfazUsuario {
+	private static Scanner scanner = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		try {
+			EcuacionSegundoGrado ecuacion = new EcuacionSegundoGrado();
+			
+			double a = leerDouble("Ingrese el coeficiente a: ");
+			double b = leerDouble("Ingrese el coeficiente b: ");
+			double c = leerDouble("Ingrese el coeficiente c: ");
+			
+
+			double[] raices = ecuacion.calcularRaices(a, b, c);
+
+			System.out.println("Las raices son:");
+			for (double raiz : raices) {
+				System.out.println(raiz);
+			}
+		} catch (Exception e) {
+			System.err.println("Error: " + e.getMessage());
+		}
+	}
+
+	public static double leerDouble(String mensaje) {
+		while (true) {
+			try {
+				System.out.print(mensaje);
+				return scanner.nextDouble();
+			} catch (InputMismatchException e) {
+				System.err.println("Error: Ingrese un valor numerico valido.");
+				scanner.nextLine();
+			}
+		}
+	}	
+}
